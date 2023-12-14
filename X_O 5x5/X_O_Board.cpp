2eaef169eb5115ff23fead151c316af7d58cbe80
Maria-alfonse <125464100+Maria-alfonse@users.noreply.github.com>
2023-12-15 01:06:22 +0200

    Add files via upload

---
+++ X_O 5x5/X_O_Board.cpp
@@ -0,0 +1,105 @@
+// Class definition for X_O_Board class
+// Author:  Mohammad El-Ramly
+// Date:    10/10/2022
+// Version: 1
+#include <iostream>
+#include <random>
+#include <iomanip>
+#include <algorithm>
+#include <map>
+#include "BoardGame_Classes.hpp"
+using namespace std;
+
+// Set the board
+X_O_Board::X_O_Board () {
+   n_rows = n_cols = 5;
+   board = new char*[n_rows];
+   for (int i = 0; i < n_rows; i++) {
+      board [i] = new char[n_cols];
+      for (int j = 0; j < n_cols; j++)
+         board[i][j] = 0;
+   }
+}
+
+// Return true  if move is valid and put it on board
+// within board boundaries in empty cell
+// Return false otherwise
+bool X_O_Board::update_board (int x, int y, char mark){
+   // Only update if move is valid
+   if (!(x < 0 || x > 4 || y < 0 || y > 4) && (board[x][y] == 0)) {
+      board[x][y] = toupper(mark);
+      n_moves++;
+      return true;
+   }
+   else
+      return false;
+}
+
+// Display the board and the pieces on it
+void X_O_Board::display_board() {
+   for (int i: {0,1,2,3,4}) {
+      cout << "\n| ";
+      for (int j: {0,1,2,3,4}) {
+         cout << "(" << i << "," << j << ")";
+         cout << setw(2) << board [i][j] << " |";
+      }
+      cout << "\n-----------------------------------------------";
+   }
+   cout << endl;
+}
+
+// Returns true if there is any winner
+// either X or O
+// Written in a complex way. DO NOT DO LIKE THIS.
+bool X_O_Board::is_winner() {
+    map<char, int>who_is_winner;
+    for (int i:{0,1,2,3,4}) {
+        if(board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+            who_is_winner[board[i][0]]++;
+        }
+        if(board[i][1] == board[i][2] && board[i][2] == board[i][3]){
+            who_is_winner[board[i][1]]++;
+        }
+        if(board[i][2] == board[i][3] && board[i][3] == board[i][4]){
+            who_is_winner[board[i][2]]++;
+        }
+        if(board[0][i] == board[1][i] && board[1][i] == board[2][i]){
+            who_is_winner[board[0][i]]++;
+        }
+        if(board[1][i] == board[2][i] && board[2][i] == board[3][i]){
+            who_is_winner[board[1][i]]++;
+        }
+        if(board[2][i] == board[3][i] && board[3][i] == board[4][i]){
+            who_is_winner[board[2][i]]++;
+        }
+    }
+    for(int i:{0,1,2}){
+        for(int j:{0,1,2}){
+            if(board[i][j] == board[i+1][j+1] && board[i+1][j+1] == board[i+2][j+2]){
+                who_is_winner[board[i][j]]++;
+            }
+        }
+    }
+    for(int i:{0,1,2}){
+        for(int j:{2,3,4}){
+            if(board[i][j] == board[i+1][j-1] && board[i+1][j-1] == board[i+2][j-2]){
+                who_is_winner[board[i][j]]++;
+            }
+        }
+    }
+    cout<<"Player X: "<<who_is_winner['X']<<'\n';
+    cout<<"Player O: "<<who_is_winner['O']<<'\n';
+
+    if(who_is_winner['X']>who_is_winner['O']){
+        cout<<"X is the winner!\n";
+    }else if(who_is_winner['X']==who_is_winner['O']){
+        cout<<"Draw!\n";
+    }else{
+        cout<<"O is the winner!\n";
+    }
+    return 0;
+}
+
+bool X_O_Board::game_is_over () {
+    return n_moves >= 24;
+}
