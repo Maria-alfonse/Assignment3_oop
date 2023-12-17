d1910541132421fe05253ac5ccb3280c312a73cd
Ahmed Abdelaziz <113646264+ahmed3ab3az@users.noreply.github.com>
2023-12-17 18:11:12 +0200

    Add files via upload

---
+++ Pyramic Tic-Tac-Toe(V3)/BoardGame_Classes.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Ahmed on 12/4/2023.
+//
+
+#include "BoardGame_Classes.h"
+#include <iostream>
+#include <string>
---
+++ Pyramic Tic-Tac-Toe(V3)/RandomPlayer.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Ahmed on 12/4/2023.
+//
+#include<iostream>
+#include<random>
+#include<iomanip>
+#include<algorithm>
+#include "BoardGame_Classes.h"
+
+
+RandomPlayer::RandomPlayer (char symbol, int dimension , int dimension2):Player(symbol)
+{
+    this->dimension = dimension;
+    this->dimension2 = dimension2;
+    this->name = "Random Computer Player";
+    cout << "My names is " << name << endl;
+}
+
+// Generate a random move
+void RandomPlayer::get_move(int& x, int& y) {
+    x = (int) (rand()/(RAND_MAX + 1.0) * dimension);
+    y = (int) (rand()/(RAND_MAX + 1.0) * (dimension2));
+}
